c7fda14d37274f71331b73063f058cee615b21e2
Jorlyna Pujols <jorlynapujols@Jorlynas-MBP.attlocal.net>
2023-11-16 15:57:24 -0500

    built out majority of CLI class and added run command

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// import any packages that we will need
-const inquirer =  require('inquirer')
+const CLI=require("./lib/cli")
+new CLI().run()
 
-// create a function that will ask the user 4 questions: 
-// 1. what shape do you want (circle, square, triangle)
-// 2. provide text for the shap (MAX 3 characters)
-// 3. provide the color of the shape
-// 4. provide the color of the text
-
-
-// once all the questions are answers we need to render the svg. HINT: we will need to verify what shape the user chose to determine which class we need to create a new instance of. (example: if the user chose circle we need to create a new instance of the class circle...)
-
-// once that it done we need to write a new file "logo.svg" with the data from the render method
